5fbd15e98d6f98285dd2e41875c35fb9345fd597
Alex <fox228.af@gmail.com>
2021-11-06 16:28:19 +0200

    .GITIGNORE, .GITATTRIBUTES

--- obj/Debug/net5.0-windows/App.g.i.cs
+++ obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace gratch_desktop {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/gratch-desktop;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/gratch-desktop;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Debug/net5.0-windows/Views/MainWindow.g.i.cs
+++ obj/Debug/net5.0-windows/Views/MainWindow.g.i.cs
@@ -66,7 +66,7 @@ namespace gratch_desktop.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/gratch-desktop;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/gratch-desktop;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

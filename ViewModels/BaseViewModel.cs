cd5793154ad56d9de2ed71ba2348ac6361cb8b5b
Alex <fox228.af@gmail.com>
2021-11-06 16:53:55 +0200

    ViewModels

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ReactiveUI;
+
+namespace gratch_desktop.ViewModels
+{
+    internal class BaseViewModel : ReactiveObject
+    {
+    }
+}

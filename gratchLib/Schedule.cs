5a40045f98d7d8d85c5c9537a269b35bc8cc8299
Alex Silchenko <mralextawer@gmail.com>
2022-06-29 20:06:27 +0300

    Schedule.ToString()

--- gratchLib/Schedule.cs
+++ gratchLib/Schedule.cs
@@ -1,8 +1,8 @@
-﻿
+﻿using System.Collections;
 
 namespace gratchLib
 {
-    public class Schedule
+    public class Schedule : IEnumerable<KeyValuePair<DateTime, Person>>
     {
         protected Dictionary<DateTime, Person> _schedule = new();
 
@@ -18,7 +18,13 @@ namespace gratchLib
 
         protected void BuildSchedule()
         {
-            
+            for (int i = 0, j = 1; i < Period.DaysSpan; i++, j++)
+            {
+                var dateToAssign = Period.ElementAt(i);
+                if (Group.Calendar.IsHoliday(dateToAssign)) continue; // skip if holiday
+                if (j > Group.ActivePeople.Count) j = 1;              // first pos on overflow
+                _schedule.Add(dateToAssign, Group.ActivePeople[j]);
+            }
         }
         protected int FirstPosition
         {
@@ -26,10 +32,27 @@ namespace gratchLib
             {
                 var peoplecount = Group.People.Count;
                 var startdate = Group.Calendar.StartDate;
+                var periodstart = Period.Start;
                 // period between first init and start of schedule's period;
-                var period = new DateTimeRange(startdate, Period.Start);
-                return ((period.DaysSpan - (Period.Start.Day-1)) % peoplecount) + 1;
+                var period = new DateTimeRange(startdate, periodstart);
+                return ((period.DaysSpan - (periodstart.Day - 1)) % peoplecount) + 1;
             }
         }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+
+            foreach (KeyValuePair<DateTime, Person> pair in this)
+            {
+                // DD.MM.YYYY : Person [Pos]
+                result += pair.Key.ToShortDateString() + " : " + pair.Value.Name + $" [{pair.Value.Position}]\n";
+            }
+
+            return result;
+        }
+
+        public IEnumerator GetEnumerator() => _schedule.GetEnumerator();
+        IEnumerator<KeyValuePair<DateTime, Person>> IEnumerable<KeyValuePair<DateTime, Person>>.GetEnumerator() => _schedule.GetEnumerator();
     }
 }

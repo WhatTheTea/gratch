4d924d26f9b5ef4650d340a3a4cfebed4fa6a625
Alex <fox228.af@gmail.com>
2022-07-27 01:45:58 +0300

    d:\WORK\ГраЧ\gratch\gratchWebApi\src\Program.cs

--- gratchWebApi/src/Program.cs
+++ gratchWebApi/src/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddControllers()
                 .AddNewtonsoftJson(options => options.SerializerSettings
                                                      .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
+//builder.Services.AddAuthentication().AddOAuth
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

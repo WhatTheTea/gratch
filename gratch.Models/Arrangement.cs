7d606d6a0af8b59ed9d95f45dace77d5c917e707
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-31 20:00:58 +0300

    Create arrangement model

---
+++ gratch.Models/Arrangement.cs
@@ -0,0 +1,16 @@
+﻿namespace WhatTheTea.Gratch.Models;
+public class Arrangement
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public List<Person> PeopleArrangement { get; set; } = [];
+
+    public Arrangement(string title)
+    {
+        this.Title = title;
+    }
+
+    public Dictionary<DateTimeOffset, Person> GenerateForPeriod(DateTimeOffset start, DateTimeOffset end)
+    {
+    }
+}

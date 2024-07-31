3b8ad2a7f58423c212bf34febd811f6ff840d692
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-31 19:17:22 +0300

    Add basic first test

---
+++ gratch.Tests/Core/ArrangementTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using FluentAssertions;
+using Microsoft.Extensions.Time.Testing;
+using WhatTheTea.Gratch.Models;
+using WhatTheTea.Gratch.Core;
+
+namespace WhatTheTea.Gratch.Tests.Core;
+public class ArrangementTests
+{
+    [Fact]
+    public void PeopleAreArrangedOneByOne()
+    {
+        var timeProvider = new FakeTimeProvider(new DateTime(2024, 07, 01));
+        var people = Enumerable.Range(1, 30).Select(x => new Person(x.ToString()));
+        var arranger = new BasicArranger(timeProvider);
+
+        arranger.ArrangeMany(people);
+
+        people.Should().Satisfy(x => x.Name == x.Shift.Day.ToString());
+    }
+}

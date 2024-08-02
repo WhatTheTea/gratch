fb0e747a8914feece2497b0c4257ffaa6bdfe583
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-08-02 18:44:15 +0300

    Add view for viewmodel abstraction

---
+++ gratch.Abstractions/IViewFor.cs
@@ -0,0 +1,9 @@
+﻿namespace WhatTheTea.Gratch.Abstractions;
+/// <summary>
+/// Interface for pages with viewmodel
+/// </summary>
+/// <typeparam name="VM">ViewModel</typeparam>
+public interface IViewFor<VM>
+{
+    public VM ViewModel { get; }
+}

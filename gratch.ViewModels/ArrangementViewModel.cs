26ce8ce10be2fb4562c7acb4b4374f1c83eed9ea
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-08-02 18:01:26 +0300

    Create arrangement viewmodel

---
+++ gratch.ViewModels/ArrangementViewModel.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WhatTheTea.Gratch.ViewModels;
+public class ArrangementViewModel : ObservableObject
+{
+    public string Title { get; set; } = string.Empty;
+}

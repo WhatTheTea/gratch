66806bbee2c4e18651f61f44cea558d17afb2ac9
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-10-16 23:34:01 +0300

    Add skip exact dates rule

---
+++ gratch.Core/Rules/SkipExactDatesRule.cs
@@ -0,0 +1,7 @@
+﻿using WhatTheTea.Gratch.Abstractions;
+
+namespace WhatTheTea.Gratch.Core.Rules;
+public class SkipExactDatesRule(IEnumerable<DateTimeOffset> dates) : IRule
+{
+    public bool Evaluate(DateTimeOffset dateTime) => !dates.Contains(dateTime);
+}

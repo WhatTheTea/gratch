3a10060b1e3a18589aa97790baac12948f4d4a47
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-10-16 23:34:08 +0300

    Add skip weekdays rule

---
+++ gratch.Core/Rules/SkipWeekDaysRule.cs
@@ -0,0 +1,7 @@
+﻿using WhatTheTea.Gratch.Abstractions;
+
+namespace WhatTheTea.Gratch.Core.Rules;
+public class SkipWeekDaysRule(IEnumerable<DayOfWeek> blacklist) : IRule
+{
+    public bool Evaluate(DateTimeOffset dateTime) => !blacklist.Contains(dateTime.DayOfWeek);
+}

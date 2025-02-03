5282910dcfcecde7414594e703bb4132acd2205c
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2025-02-03 23:48:19 +0200

    Add arrangement related models

--- domain/gratch.Models/Group.cs
+++ domain/gratch.Models/Group.cs
@@ -3,4 +3,8 @@
 public class Group : List<Person>
 {
     public string Name { get; set; } = string.Empty;
+
+    public DateTimeOffset BaseDateTimeOffset { get; set; }
+
+    public ArrangementConfiguration ArrangementConfiguration { get; set; } = new();
 }

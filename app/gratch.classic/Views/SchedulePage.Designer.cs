00c87f6dd3639742f3a20fe309f05b02d4403e35
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2025-05-11 18:30:10 +0300

    Implement schedule datagrid design

--- app/gratch.classic/Views/SchedulePage.Designer.cs
+++ app/gratch.classic/Views/SchedulePage.Designer.cs
@@ -80,10 +80,10 @@ partial class SchedulePage
         // 
         this.scheduleDataGrid.AllowUserToAddRows = false;
         this.scheduleDataGrid.AllowUserToDeleteRows = false;
-        this.scheduleDataGrid.AllowUserToResizeColumns = false;
         this.scheduleDataGrid.AllowUserToResizeRows = false;
         this.scheduleDataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
-        this.scheduleDataGrid.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllHeaders;
+        this.scheduleDataGrid.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+        this.scheduleDataGrid.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
         this.scheduleDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
         this.tableLayoutPanel1.SetColumnSpan(this.scheduleDataGrid, 2);
         this.scheduleDataGrid.Dock = DockStyle.Fill;
@@ -91,6 +91,7 @@ partial class SchedulePage
         this.scheduleDataGrid.Margin = new Padding(12);
         this.scheduleDataGrid.Name = "scheduleDataGrid";
         this.scheduleDataGrid.ReadOnly = true;
+        this.scheduleDataGrid.RowHeadersVisible = false;
         this.scheduleDataGrid.ShowEditingIcon = false;
         this.scheduleDataGrid.Size = new Size(444, 325);
         this.scheduleDataGrid.TabIndex = 2;
--- app/gratch.classic/Views/SchedulePage.cs
+++ app/gratch.classic/Views/SchedulePage.cs
@@ -1,4 +1,5 @@
-﻿using System.Reactive.Linq;
+﻿using System;
+using System.Reactive.Linq;
 
 using gratch.ViewModels;
 
@@ -12,11 +13,29 @@ namespace gratch.classic.Views;
 [IViewFor<ScheduleViewModel>]
 public partial class SchedulePage : UserControl
 {
+    private static DateTime MonthStart => new(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+    private static int MonthDays => DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+
+    private static IEnumerable<DateTime> MonthDates =>
+        Enumerable.Range(0, MonthDays)
+            .Select(d => MonthStart.AddDays(d));
+
+    private static readonly DataGridViewCellStyle CurrentDateCellStyle = new()
+    {
+        BackColor = Color.BlueViolet
+    };
+
     public SchedulePage()
     {
         this.InitializeComponent();
+        this.InitializeScheduleDataGrid();
         this.ViewModel = Ioc.Container.GetService<ScheduleViewModel>();
+        this.InitializeBindings();
+    }
 
+    private void InitializeBindings()
+    {
         this.OneWayBind(this.ViewModel, x => x.Groups, x => x.groupsComboBox.DataSource);
 
         this.groupsComboBox.DisplayMember = "Name";
@@ -25,15 +44,39 @@ public partial class SchedulePage : UserControl
             .Select(_ => this.groupsComboBox.SelectedItem)
             .BindTo(this.ViewModel, vm => vm.SelectedGroup);
 
-        var now = DateTime.Now;
-        var firstDayOfMonth = new DateTime(now.Year, now.Month, 1);
-        var daysInMonth = DateTime.DaysInMonth(now.Year, now.Month);
-        var dates = Enumerable.Range(0, daysInMonth)
-                  .Select(d => firstDayOfMonth.AddDays(d));
-        foreach (var date in dates)
-        {
-            this.scheduleDataGrid.Columns.Add(date.Day.ToString(), date.Day.ToString());
-        }
+        this.ViewModel.WhenAnyValue(x => x.Arrangement)
+            .WhereNotNull()
+            .Subscribe(this.UpdateScheduleDataGrid);
+    }
 
+    private void InitializeScheduleDataGrid()
+    {
+        this.scheduleDataGrid.Columns.Add("Name", "Name");
+        foreach (var date in MonthDates)
+        {
+            var columnIndex = this.scheduleDataGrid.Columns.Add(date.Day.ToString(), date.Day.ToString());
+            var column = this.scheduleDataGrid.Columns[columnIndex];
+            column.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            column.Width = 25;
+        }
+    }
+
+    private void UpdateScheduleDataGrid(Models.Arrangement arrangement)
+    {
+        this.scheduleDataGrid.Rows.Clear();
+
+        foreach (var person in this.ViewModel?.SelectedGroup?.People ?? [])
+        {
+            var name = person.Name;
+            var rowIndex = this.scheduleDataGrid.Rows.Add(name);
+
+            var arrangedDays = MonthDates.Where(x => person == arrangement[x])
+                .Select(x => x.Day.ToString());
+
+            foreach (var day in arrangedDays)
+            {
+                this.scheduleDataGrid[day, rowIndex].Style = CurrentDateCellStyle;
+            }
+        }
     }
 }
